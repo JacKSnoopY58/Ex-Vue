c7fc982a3b61721775b4b7eff8401d240e76571c
JacKSnoopY58 <titijack58@gmail.com>
2024-04-11 12:05:56 +0700

    update

--- src/views/Apicon.vue
+++ src/views/Apicon.vue
@@ -1,29 +1,121 @@
 <template>
-  <div>
-    <h1>Apicon Page</h1>
-    <h1>{{ apidata }}</h1>
-    <v-btn color="success" @click="getData()">call api</v-btn>
-  </div>
-</template>
-
-<script>
-export default {
-data(){
-    return{
-        apidata: []
-    }
-},
-    methods:{
-        getData(){
-            this.axios.get('http://localhost:3000/api/v1/orders').then((response) => {
-                console.log('data: ',response.data.data)
-                this.apidata = response.data.data;
-            })
+    <div>
+      <h1>Apicon Page</h1>
+  
+      <v-row>
+        <v-col cols="3" v-for="(item, index) in apidata" :key="index">
+          <v-img :src="item.img" height="300"/>
+          <v-img src="https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/www-instagram-com-monkeycatluna-hl-851711797.jpg" height="250" width="250"></v-img>
+          <v-card-title primary-title>
+            {{ item.Pname }}
+          </v-card-title>
+          <v-card-actions>
+            <v-btn color="blue" @click="editItem(item)">Edit</v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+  
+      <v-dialog v-model="dialogedit" max-width="500px">
+        <v-card>
+          <v-card-title>
+            {{ savemode }}
+          </v-card-title>
+          <v-col cols="6">
+            <v-text-field
+              name="Productname"
+              label="Productname"
+              id="Productname"
+              v-model="postdata.Pname">
+            </v-text-field>
+  
+            <v-text-field
+              name="Productprice"
+              label="Productprice"
+              id="price"
+              v-model="postdata.price">
+            </v-text-field>  
+          </v-col>
+          <v-btn color="success" @click="displayAlert(postdata.Pname)">Save</v-btn>
+          <v-btn color="red" @click="toggleName()">Cancel</v-btn>
+        </v-card>
+      </v-dialog>
+  
+      <v-btn color="success" @click="getData()">Call API</v-btn>
+      <v-btn color="success" @click="newItem()">Create</v-btn>
+        
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        id:'',
+        apidata: [],
+        dialogedit: false, 
+        postdata: {
+          Pname: '',
+          price: '',
+          stock: ''
+        },
+        postdata2: {
+          Pname: '',
+          price: '',
+          stock: ''
+        },
+      };
+    },
+    computed: {
+      savemode() {
+        return this.id === '' ? 'NewItem' : 'Edit Item';
+      }
+    },
+    created() {
+      this.getData();
+    },
+    methods: {
+      getData() {
+        this.axios.get('http://localhost:3000/api/v1/product')
+          .then(response => {
+            console.log('data:', response.data.data);
+            this.apidata = response.data.data;
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+      },
+      newItem() {
+        this.id = '';
+        this.postdata = { ...this.postdata2};
+        this.dialogedit = true;
+      },
+      editItem(item) {
+        this.id = item._id;
+        this.postdata = { ...item};
+        this.dialogedit = true;
+      },
+      closeItem(item) {
+        this.id = item._id;
+        this.postdata = { ...item};
+        this.dialogedit = false;
+      },
+      displayAlert(username) {
+        alert(username);
+      },
+      toggleName() {
+        this.dialogedit = !this.dialogedit;
+      },
+      saveSelect () {
+        if (this.id !== '') {
+          alert('update');
+        } else {
+          alert('new item');
         }
+      }
     }
-}
-</script>
-
-<style>
-
-</style>
+  }
+  </script>
+  
+  <style>
+  </style>
+  
--- src/views/Simple.vue
+++ src/views/Simple.vue
@@ -1,56 +1,69 @@
 <template>
-    <div>
-      <h1>Simple</h1>
-    
-      <h1 v-if="check">{{ name }}</h1>
-      <v-btn color="blue" @click="check = !check">Show</v-btn>
-      <v-btn color="blue" @click="check = display()">Hello</v-btn>
-      <v-row>
-          <v-col cols="3" v-for="(item, index) in imagestock" :key="index">
-            <v-img :src="item.img" height="300"/>
-            <v-card-title primary-title>
-                {{ item.message }}
-            </v-card-title>
-            <v-card-actions>
-                <v-btn color="blue" @click="disPlayAlert(item.message)">Name</v-btn>
-            </v-card-actions>
-          </v-col>
-          <simchild @display = "display" :name = "name"/>
-      </v-row>
-    </div>
-  </template>
-  
-  <script>
-    import { EventBus } from '@/EventBus'
-  import simchild from '../components/SimpleChild.vue'
-  export default {
-    components: {simchild},
-    data() {
-      return {
-        name: "Ittikorn",
-        check: true,
-        imagestock: [
-          { message: 'Foo', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ZQha6YdHqW2SKEFo9p2ltHvnDRIps-3w7Q7RAWEluw&s' },
-          { message: 'Bar', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGTeq1b0nqicQ4euFmdQaTN2QbYVyB2VnmO_I9gPq6Vw&s' }
-        ]
-      };
-    },
-    mounted(){
-        EventBus.$on('test1', this.display);
-    },
-    beforeDestroy(){
-        EventBus.$off('test2', this.display);
-    },
-    methods:{
-        display(){
-            alert('Hello')
+  <div>
+    <h1>Simple</h1>
+
+    <h1 v-if="check">{{ name }}</h1>
+    <v-btn color="blue" @click="toggleName">Show</v-btn>
+    <v-btn color="blue" @click="displayHello">Hello</v-btn>
+    <v-row>
+      <v-col cols="3" v-for="(item, index) in imagestock" :key="index">
+        <v-img :src="item.img" height="300" />
+        <v-card-title primary-title>
+          {{ item.message }}
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn color="blue" @click="disPlayAlert(item.message)">Name</v-btn>
+        </v-card-actions>
+      </v-col>
+      <simchild :name="name" />
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { EventBus } from "@/EventBus";
+import simchild from "../components/SimpleChild.vue";
+
+export default {
+  components: { simchild },
+  data() {
+    return {
+      name: "Ittikorn",
+      check: true,
+      imagestock: [
+        {
+          message: "Foo",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ZQha6YdHqW2SKEFo9p2ltHvnDRIps-3w7Q7RAWEluw&s",
         },
-        disPlayAlert(item){
-            alert('Name: '+ item)
-        }
-    }
-  };
-  </script>
-  
-  <style></style>
-  
+        {
+          message: "Bar",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGTeq1b0nqicQ4euFmdQaTN2QbYVyB2VnmO_I9gPq6Vw&s",
+        },
+      ],
+    };
+  },
+  mounted() {
+    EventBus.$on("test1", this.display);
+  },
+  beforeDestroy() {
+    EventBus.$off("test1", this.display);
+  },
+  methods: {
+    displayHello() {
+      EventBus.$emit("test1");
+    },
+    disPlayAlert(item) {
+      alert("Name: " + item);
+    },
+    toggleName() {
+      this.check = !this.check;
+    },
+    display() {
+      alert("Hello");
+    },
+  },
+};
+</script>
+
+<style></style>
